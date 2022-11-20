a3c7906f25dd4c598259710029c7101c2a4ce1f1
Bakhtiyor Yusupov <b.yusupoff001@gmail.com>
2022-11-20 16:25:28 +0900

    Update Rules.java

--- src/Rules.java
+++ src/Rules.java
@@ -6,7 +6,7 @@ public class Rules {
 
         System.out.println(">>> Consider you became a trader. You have $200 on your account balance at the start of the game. You plan to increase that as much as possible by trading the stocks of companies listed on the stock market.\n");
 
-        System.out.println(">>> Every round is initiated by displaying a listing of all stocks available on the market. Each one has got its full name, shorter index and price, depicted with how it has changed compared to previous round. Right below that you will see the list of stocks currently included in your portfolio and your current account balance.\n");
+        System.out.println(">>> Every round is initiated by displaying a listing of all stocks available on the market. Each one has got its full name and price, depicted with how it has changed compared to previous round. Right below that you will see the list of stocks currently included in your portfolio and your current account balance.\n");
 
         System.out.print("[Press ENTER to continue...]");
 
